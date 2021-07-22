f824abd69edbf19dc505318bd4a8488dc333e2e7
alexandr.vatutin@gmail.com <ShmiTT6887>
2021-07-22 17:30:39 +0300

    first

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,14 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        int hours = seconds / 3600;
+        int minutes = seconds % 3600 / 60;
+        int leftSeconds = seconds - hours * 3600 - minutes * 60;
+
+        System.out.println(hours + ":" + minutes + ":" + leftSeconds);
+
+
+
 
     }
 }
