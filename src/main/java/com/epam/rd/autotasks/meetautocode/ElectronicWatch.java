9ba8bbe73903b96fcdc47fe58d3ce4c124fbad3e
alexandr.vatutin@gmail.com <ShmiTT6887>
2021-07-23 10:11:06 +0300

    first

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -1,6 +1,7 @@
 package com.epam.rd.autotasks.meetautocode;
 
 import java.util.Scanner;
+import java.lang.String;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
@@ -11,10 +12,13 @@ public class ElectronicWatch {
         int minutes = seconds % 3600 / 60;
         int leftSeconds = seconds - hours * 3600 - minutes * 60;
 
-        System.out.println(hours + ":" + minutes + ":" + leftSeconds);
+        String h = hours + "";
+        String m = minutes + "";
+        String ls = leftSeconds + "";
 
+//        String str = String.format("%tI:%tM:%tS", hours, minutes, leftSeconds);
 
-
-
+//        System.out.printf("%d:%0d:%0d", hours, minutes, leftSeconds);
+        System.out.println(("0" + h).substring(h.length()) + ":" + ("00" + m).substring(m.length()) + ":" + ("00" + ls).substring(ls.length()));
     }
 }
